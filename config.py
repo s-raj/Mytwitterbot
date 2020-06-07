6dcd0e1bd5af80da5fd05189bb0799e9226958f7
s-raj <65283588+s-raj@users.noreply.github.com>
2020-06-08 01:44:44 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#config.py
+
+'''
+Configuration Enum
+'''
+ERROR = 0
+WARNING = 1
+INFO = 2
+
+'''
+Parameters
+'''
+LOGLVL = INFO
+RANDOM_TWEET_TXT_FILE = 'tweet.txt' # Your random tweet text file, one quote/ info per line formatting!
+RANDOM_TWEET_DAYS = 1
+RANDOM_TWEET_HOUR = 7
+RANDOM_TWEET_MINUTE = 0
+RANDOM_TWEET_SEC = 0
+RANDOM_TWEET_MSEC = 0
+DM_POLL_FREQ_IN_HOUR = 20
