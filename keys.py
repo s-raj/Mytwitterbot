8a2b1467a5db8f6f37729778922905aa51a558e6
s-raj <65283588+s-raj@users.noreply.github.com>
2020-06-08 01:46:21 +0200

    Create keys.py

---
+++ keys.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#keys.py
+keys = dict(
+	apiKey = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+	apiSecret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+	accessToken = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+	accessTokenSecret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+)
+
+TWITTER_SENDER_ID = 'xxxxx' #Your twitter id
